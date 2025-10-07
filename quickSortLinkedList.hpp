64661fd2e116b817d5464bf4c8b33660bdea833d
audible-sound <110619036+audible-sound@users.noreply.github.com>
2025-10-08 01:06:57 +0700

    Delete quickSortLinkedList.hpp

--- quickSortLinkedList.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "linkedList.hpp"
-
-SkillNode* partition(SkillNode* head, SkillNode* end, SkillNode** newHead, SkillNode** newEnd);
-SkillNode* quickSort(SkillNode* head, SkillNode* end);
-SkillNode* getTail(SkillNode* tail);
